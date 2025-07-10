ed308e05a012220064dc5543bd406a7cf51465e5
Metroid <159981647+aaronstarstaff@users.noreply.github.com>
2025-07-10 10:32:49 -0700

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,6 +5,6 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-add_lunch_combo omni_gts10fepwifi-user
-add_lunch_combo omni_gts10fepwifi-userdebug
-add_lunch_combo omni_gts10fepwifi-eng
+add_lunch_combo twrp_gts10fepwifi-user
+add_lunch_combo twrp_gts10fepwifi-userdebug
+add_lunch_combo twrp_gts10fepwifi-eng
